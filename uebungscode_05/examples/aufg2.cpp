40308fb319b5552071c9d156a067f06727710bff
Arthur Sliwa <sliwa@uni-bonn.de>
2023-11-08 16:09:37 +0100

    funzt nicht

--- uebungscode_05/examples/aufg2.cpp
+++ uebungscode_05/examples/aufg2.cpp
@@ -3,20 +3,17 @@
 #include <hanoi/hanoi.hpp>
 
 int main(int, char**) {
-  // hanoi::Hanoi h(5);
-  // h.drucke();
-  // 
-  // h.bewege(0,1);
-  // h.bewege(0,1); // Illegale Bewegung
-  // h.bewege(0,2);
-  // h.drucke();
-  // 
-  // h.bewege(1,2);
-  // h.drucke();
-  // 
-  // h.bewege(0,1);
-  // h.bewege(2,0);
-  // h.bewege(2,1);
-  // h.bewege(0,1);
-  // h.drucke();
+  hanoi::Hanoi h(5);
+  h.drucke();
+  h.bewege(0,1);
+  h.bewege(0,1); // Illegale Bewegung
+  h.bewege(0,2);
+  h.drucke();
+  h.bewege(1,2);
+  h.drucke();
+  h.bewege(0,1);
+  h.bewege(2,0);
+  h.bewege(2,1);
+  h.bewege(0,1);
+  h.drucke();
 }
