4d35c04af547aced5b4fd49c9c06c33f63937a29
raziaSO <151096192+raziaSO@users.noreply.github.com>
2023-11-20 19:53:03 +0100

    Update aufg3.cpp

--- uebungscode_06/examples/aufg3.cpp
+++ uebungscode_06/examples/aufg3.cpp
@@ -1,8 +1,26 @@
-#include <simpler_graph/simpler_graph.hpp>
+#include "simpler_graph.hpp"
 
 #include <iostream>
 
 
 int main() {
-  // ...
+    SimplerGraph::SimplerGraph graph(5);
+
+    // Kanten hinzufügen
+    graph.addKante(0, 1);
+    graph.addKante(1, 2);
+    graph.addKante(2, 3);
+
+    // Graph ausgeben
+    std::cout << "Graph nach dem Hinzufügen von Kanten:" << std::endl;
+    graph.printGraph();
+
+    // Kante entfernen
+    graph.removeKante(1, 2);
+
+    // Graph erneut ausgeben
+    std::cout << "Graph nach Entfernen von Kanten: " << std::endl;
+    graph.printGraph();
+
+    return 0;
 }
