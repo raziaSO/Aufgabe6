c18396c8b0f86bc175ed393f91a6c81b65b218f8
raziaSO <151096192+raziaSO@users.noreply.github.com>
2023-11-20 19:51:14 +0100

    Update aufg1.cpp

--- uebungscode_06/examples/aufg1.cpp
+++ uebungscode_06/examples/aufg1.cpp
@@ -1,11 +1,22 @@
-#include <mergesort.hpp>
-#include <misc/hilfsfunktionen.hpp>
+#include "mergesort.hpp"
+#include "hilfsfunktionen.hpp"
 #include <vector>
 #include <iostream>
 
 int main() {
-  std::vector<int> bsp_zahlen(4000);
-  misc::fuelle_zufaellig(bsp_zahlen);
-  misc::drucke_array(bsp_zahlen);
-  
+    std::vector<int> bsp_zahlen(4000);
+    misc::fuelle_zufaellig(bsp_zahlen);
+
+    // Ausgabe der ursprünglichen Zahlen
+    std::cout << "Unsortiertes Array:";
+    misc::drucke_array(bsp_zahlen);
+
+    // Mergesort anwenden
+    misc::mergesort(bsp_zahlen, 0, bsp_zahlen.size() - 1);
+
+    // Ausgabe der sortierten Zahlen
+    std::cout << "Sortiertes Array:";
+    misc::drucke_array(bsp_zahlen);
+
+    return 0;
 }
