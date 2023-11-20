019965d6ee4d20854d60e2cc20b325d81ec1fd00
raziaSO <151096192+raziaSO@users.noreply.github.com>
2023-11-20 19:45:37 +0100

    Update simpler_graph.hpp

--- uebungscode_06/include/simpler_graph/simpler_graph.hpp
+++ uebungscode_06/include/simpler_graph/simpler_graph.hpp
@@ -3,15 +3,25 @@
 #include <vector>
 #include <cstddef>
 
-namespace SimplerGraph{
-  
-  class SimplerGraph{
-  public:
-    // Erstellt einen Graph mit einer festen Knotenanzahl
-    SimplerGraph(size_t knoten_anzahl);
-    
+namespace SimplerGraph {
 
-  private:
-    const size_t knoten_anz;
-  };
+    class SimplerGraph {
+    public:
+        // Erstellt einen Graph mit einer festen Knotenanzahl
+        SimplerGraph(size_t knoten_anzahl);
+
+        // Fügt dem Graphen eine Kante hinzu
+        void addKante(size_t from_Knoten, size_t to_Knoten);
+
+        // Entfernt eine Kante aus dem Graphen
+        void removeKante(size_t from_Knoten, size_t to_Knoten);
+
+        // Gibt den Graphen aus
+        void printGraph() const;
+
+    private:
+        const size_t knoten_anz;
+        std::vector<std::vector<bool>> graph;
+    };
+
 }
