d65e7dde39eba373d94b896545bf9dc316f3568b
raziaSO <151096192+raziaSO@users.noreply.github.com>
2023-11-20 19:52:27 +0100

    Update aufg2.cpp

--- uebungscode_06/examples/aufg2.cpp
+++ uebungscode_06/examples/aufg2.cpp
@@ -1,14 +1,36 @@
-#include <misc/zeitmesser.hpp>
-#include <misc/schnellsort.hpp>
-#include <misc/hilfsfunktionen.hpp>
+#include "zeitmesser.hpp"
+#include "schnellsort.hpp"
+#include "hilfsfunktionen.hpp"
 #include <iostream>
 #include <algorithm>
 
 
 int main() {
-  std::vector<int> bsp_zahlen(4000);
-  misc::fuelle_zufaellig(bsp_zahlen);
-  misc::drucke_array(bsp_zahlen);
-  misc::schnellsort(bsp_zahlen);
-  misc::drucke_array(bsp_zahlen);
+    std::vector<int> bsp_zahlen(4000);
+    misc::fuelle_zufaellig(bsp_zahlen);
+
+    // Ausgabe der ursprünglichen Zahlen
+    std::cout << "Unsortierte Zahlen: ";
+    misc::drucke_array(bsp_zahlen);
+
+    misc::Zeitmesser zeitmesser;
+
+    // Zeitmessung starten
+    zeitmesser.start();
+    misc::schnellsort(bsp_zahlen);
+    // Zeitmessung stoppen
+    zeitmesser.stop();
+
+    // Ausgabe der sortierten Zahlen
+    std::cout << "Sortierte Zahlen: ";
+    misc::drucke_array(bsp_zahlen);
+
+    // Ausgabe der gemessenen Zeit
+    std::cout << "Dauer: " << zeitmesser.get_minutes() << " Minuten, "
+              << zeitmesser.get_seconds() << " Sekunden, "
+              << zeitmesser.get_milliseconds() << " Millisekunden,und "
+              << zeitmesser.get_microseconds() << " Mikrosekunden.";
+              
+
+    return 0;
 }
